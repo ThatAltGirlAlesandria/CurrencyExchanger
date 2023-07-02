79fcb5f813ccf3720631477179b1924e35b39ec8
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-02 15:51:35 -0700

    added full function for API call.

--- src/currencyExchange.js
+++ src/currencyExchange.js
@@ -1,4 +1,16 @@
 export default class Currency {
-  static async exchange()
-    return fetch(`https://v6.exchangerate-api.com/v6/`)
+  static async exchange(){
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`)
+      .then(function (response){
+        if(!response.ok) {
+          let errorResponse = `${response.status} ${response.statusText}`;
+          throw new Error(errorResponse);
+        } else {
+          return response.json();
+        }
+      })
+    .catch(function (error) {
+      return error;
+    })
+  }
 }
