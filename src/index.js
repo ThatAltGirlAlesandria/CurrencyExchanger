e28830dc9f90fd0fd2ad38e14d7075ca0d1fcca5
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-02 19:24:12 -0700

    Decided to re-write Business Logic and UI.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,5 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import {} from '.currencyExchange.js';
-import {} from '.currencyExchange.js';
+import {Currency} from '.currencyExchange.js';
 
-function currencyExchange(usdBase) {
-  Currency.currencyExchange()
-    .then(function(response) {
-      if (response.usdConversion) {
-        printResponse(response);
-      } else {
-        throwError(response);
-      }
-    });
-}
-
-function printResponse(response) {
-  let amount = document.querySelector('#USD').ariaValueMax;
-  let secondCurrency = document.querySelector('second-currency');
-}
