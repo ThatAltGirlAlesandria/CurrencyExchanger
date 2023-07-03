6bd55bef51ad4425b71a4c995f99e9b66875ae77
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-07-02 22:32:56 -0700

    red test. Fetch is not defined.

--- __tests__/currencyExchange.test.js
+++ __tests__/currencyExchange.test.js
@@ -1,7 +1,12 @@
-import {CurrencyExchange} from './../src/currencyExchange.js';
+import {CurrencyExchange} from '../src/currencyExchange.js';
 
 describe('CurrencyExchange', () => {
-  test('Should be able to exchange USD to the users preference.', () => {
-    expect(exchange().usd_conversion.NZD).toEqual(1.63);
+  test(`Should be able to exchange USD to the user's preference`, () => {
+    const inputUSD = 1;
+
+    return CurrencyExchange.exchange(inputUSD)
+      .then((response) => {
+        expect(response.usd_conversion.NZD).toEqual(1.63);
+      });
   });
 });
